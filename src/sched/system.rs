a7d0402e9e63e741875055d33bea028889bcf65c
Sebastian <thesicrs@gmail.com>
2020-09-05 19:48:01 +0700

    Implement Value Iteration
    Set up probability distribution
    
    Signed-off-by: Sebastian <thesicrs@gmail.com>

--- src/sched/system.rs
+++ src/sched/system.rs
@@ -1,3 +1,9 @@
+const P_DISTRIB: [[f64; 3]; 3] = [
+    [0.1, 0.5, 0.4],
+    [0.2, 0.4, 0.4],
+    [0.2, 0.4, 0.4],
+];
+
 pub struct System {
     pub state: SystemState,
     values: [f64; 3],
@@ -13,6 +19,36 @@ impl System {
             values: [0.0; 3],
             rewards,
         }
+    }
+
+    pub fn init(mut self, discount_factor: f64) -> System {
+        let new_vals  =self.calculate_values(discount_factor);
+        self.values = new_vals;
+
+        self
+    }
+
+    /// NOTE: Don't forget to acquire a WriteLock on System 
+    /// Implements Value Iteration
+    fn calculate_values(&self, discount_factor: f64) -> [f64; 3] {
+        let mut buf: [f64; 3] = [0.0; 3];
+
+        // iterate for each of the values
+        (0..self.values.len()).for_each(|_index| {
+            let val = &P_DISTRIB[_index]
+                .iter()
+                .zip(0..3) // destination node index
+                .map(|(p, idx)| p * (self.rewards[idx] + discount_factor * self.values[idx]))
+                .max_by(|a, b| a.partial_cmp(b).unwrap());
+            
+            if let Some(valu) = val {
+                buf[_index] = *valu;
+            } else {
+                panic!("Zero length output");
+            }
+        });
+
+        buf
     }
 }
 
