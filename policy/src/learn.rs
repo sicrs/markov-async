ebbd34c11be68a6e696da4f0dda725d5db2eaefa
Sebastian <thesicrs@gmail.com>
2020-09-16 00:00:46 +0700

    Initial writeup of policy-augmenting neural net
    Signed-off-by: Sebastian <thesicrs@gmail.com>

---
+++ policy/src/learn.rs
@@ -0,0 +1,89 @@
+#![allow(dead_code)]
+use peroxide::prelude::*;
+use peroxide::*;
+
+pub trait Logistic {
+    fn func(&self, input: &Matrix) -> Matrix;
+    fn deriv(&self, input: &Matrix) -> Matrix;
+}
+
+pub struct Network<Log: Logistic> {
+    logistic: Box<Log>,
+    weights: [Matrix; 3],
+    biases: [Matrix; 3],
+}
+
+impl<L: Logistic> Network<L> {
+    pub fn new(log: L) -> Network<L> {
+        let weights: [Matrix; 3] = [
+            matrix(vec![1.0; 15], 3, 5, Row),
+            matrix(vec![1.0; 6], 2, 3, Row),
+            matrix(vec![1.0; 6], 3, 2, Row),
+        ];
+
+        let biases: [Matrix; 3] = [
+            matrix(vec![0.0; 3], 3, 1, Row),
+            matrix(vec![0.0; 2], 2, 1, Row),
+            matrix(vec![0.0; 3], 3, 1, Row),
+        ];
+
+        Network {
+            logistic: Box::new(log),
+            weights,
+            biases,
+        }
+    }
+
+    /// runs the input through the network
+    pub fn passthrough(&self, input: Matrix) -> Matrix {
+        let mut accumulator = input;
+
+        for index in 0..3 {
+            let weighted_in: Matrix = &self.weights[index] * &accumulator;
+            let result: Matrix = &weighted_in + &self.biases[index];
+            drop(weighted_in);
+
+            let res = self.logistic.func(&result);
+            drop(result);
+
+            accumulator = res;
+        }
+
+        accumulator
+    }
+
+    pub fn construct_input_matrix(weighted_values: Matrix, n_im: usize, n_q: usize) -> Matrix {
+        // construct the input vector
+        // weighted_values is a horizontal 1x3 matrix
+        let w_v_vec: Vec<f64> = weighted_values.row(0);
+        drop(weighted_values);
+
+        let construct = matrix(c!(vec![n_im as f64, n_q as f64]; w_v_vec), 5, 1, Row);
+
+        construct
+    }
+
+    pub fn feed(&self, input: &Matrix, layer: usize) -> (Matrix, Matrix) {
+        let weighted_in: Matrix = &self.weights[layer] + input;
+        let result: Matrix = &weighted_in + &self.biases[layer];
+        
+        let activation = self.logistic.func(&result);
+
+        (result, activation)
+    }
+
+    // returns a tuple of accumulated inputs and activations (in that order)
+    pub fn accumulate_activ_input(&self, input: Matrix) -> (Vec<Matrix>, Vec<Matrix>) {
+        let mut inputs: Vec<Matrix> = Vec::new();
+        let mut activations: Vec<Matrix> = Vec::new();
+
+        inputs.push(input);
+        (0..3).for_each(|index| {
+            let (out, actv) = self.feed(&inputs[inputs.len() - 1], index); 
+            inputs.push(out);
+            activations.push(actv);
+        });
+
+        (inputs, activations)
+    }
+}
